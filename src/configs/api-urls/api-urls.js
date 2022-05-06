1faff8871f7f5e2c2f78066cc59df7c3f6aae497
George Apraksin <dreamvibe@mail.ru>
2022-05-06 15:49:47 +0500

    code: added deployment configurations

--- src/configs/api-urls/api-urls.js
+++ src/configs/api-urls/api-urls.js
@@ -1,4 +1,4 @@
 export const BACKEND_API_URL =
   process.env.REACT_APP_ENVIRONMENT === "development"
-    ? "http://192.168.0.103:8888/api/v1"
-    : "/";
+    ? "http://localhost:8888/api/v1"
+    : "/api/v1";
