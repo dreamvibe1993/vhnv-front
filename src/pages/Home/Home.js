4923a029aa030219fa3f6f6308c3577e9982ce0a
George Apraksin <dreamvibe@mail.ru>
2022-05-22 13:37:15 +0500

    style: hiding about section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,48 @@
-import { StackDivider, VStack } from "@chakra-ui/react";
+import { Button, Center, Collapse, Divider, StackDivider, VStack } from "@chakra-ui/react";
 import React from "react";
+import { FaChevronDown } from "react-icons/fa";
 import { About } from "../../ui/sections/About/About";
 import { BlogsSection } from "../../ui/sections/BlogsSection/BlogsSection";
 import { SongsNavigation } from "../../ui/sections/SongsSection/SongsSection";
 
 export const Home = () => {
+  const [isAboutSectionOpen, setAboutSectionOpen] = React.useState(false);
+  const [isAboutOpenByUser, setAboutOpenByUser] = React.useState(false);
+
+  const toggleAboutSectionOnUser = () => {
+    setAboutOpenByUser((prev) => !prev);
+    setAboutSectionOpen((prev) => !prev);
+  };
+
+  const toggleAboutSectionOnScroll = (isAboutOpenByUser) => {
+    if (isAboutOpenByUser) return;
+    if (window.scrollY === 0) setAboutSectionOpen(true);
+    else setAboutSectionOpen(false);
+  };
+
+  React.useEffect(() => {
+    const openAboutSectionIfScrOnTop = () => {
+      toggleAboutSectionOnScroll(isAboutOpenByUser);
+    };
+    window.addEventListener("scroll", openAboutSectionIfScrOnTop);
+    return () => {
+      window.removeEventListener("scroll", openAboutSectionIfScrOnTop);
+    };
+  }, [isAboutOpenByUser]);
+
   return (
-    <VStack divider={<StackDivider borderColor="gray.200" />} align="stretch" minH="calc(100vh - 60px - 64px)">
-      <About />
+    <VStack align="stretch" minH="calc(100vh - 60px - 64px)">
+      <Collapse in={isAboutSectionOpen} animateOpacity>
+        <About />
+      </Collapse>
+      <Collapse in={!isAboutSectionOpen} animateOpacity>
+        <Center onClick={toggleAboutSectionOnUser} cursor="pointer">
+          <FaChevronDown />
+        </Center>
+      </Collapse>
+      <Divider borderColor="gray.200" />
       <SongsNavigation />
+      <Divider borderColor="gray.200" />
       <BlogsSection />
     </VStack>
   );
